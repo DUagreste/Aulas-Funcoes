c41589a9fa83705d35f68254a7f75dd4a814997c
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-02-01 20:23:08 -0300

    Básico sobre funções.

--- Funcoes.py
+++ Funcoes.py
@@ -1,1 +1,18 @@
-# Funções
+# Funções - paradigma imperativo
+# Variáveis, atribuições e sequência
+
+# bloco externo
+nome = "Gabriel"    # variável global
+
+
+def minha_funcao():  # bloco interno *corpo da função logo abaixo
+    nome = "Ana"    # variável local
+    tupla = 2, 5, 6, 7, 8
+    print(nome)
+    print(tupla)
+    for x in tupla:
+        print(x)
+
+
+minha_funcao()
+print(nome)

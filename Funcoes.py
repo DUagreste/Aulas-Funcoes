9e0067237a0c4e0910a4a8dff49385edae909b7c
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-02-02 22:45:26 -0300

    Atualização aulas sobre Funções.

--- Funcoes.py
+++ Funcoes.py
@@ -1,11 +1,8 @@
 # Funções - paradigma imperativo
 # Variáveis, atribuições e sequência
 
-lista = [1, 2, 3, 4, 5]
-print(lista)
+def ola_mundo():
+    return "Olá, mundo"
 
-var_1 = lista.pop()
-var_2 = print("Olá, mundo!")
-print(lista)
-print("Retorno da função pop:", var_1)
-print("Retorno da função print:", var_2)
+retorno = ola_mundo()
+print(retorno)

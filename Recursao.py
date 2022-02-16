54cb2f1bec13c97c4ca62665be263d940915df4c
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-02-16 19:24:30 -0300

    Função: Recursão

---
+++ Recursao.py
@@ -0,0 +1,23 @@
+numeroInt = 5
+
+
+"""def reduzirN(numeroInt):
+    while numeroInt > 0:
+        print(numeroInt)
+        numeroInt -= 1
+
+
+reduzirN(10)"""
+
+"""
+1 - checar se o nosso número não é 0
+2 - caso não, reduzir 1
+
+5 - 1 = 4
+4 - 1 = 3
+3 - 1 = 2
+2 - 1 = 1
+1 - 1 = 0
+"""
+def reduzirNumber(nInt):
+    
